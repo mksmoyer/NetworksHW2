fcab5570ba79cf2e9608ec929deba03bcb80ee1b
mksmoyer <mks666@nyu.edu>
2024-03-13 14:00:53 -0400

    saw8

--- src/host/stop_and_wait_host.py
+++ src/host/stop_and_wait_host.py
@@ -47,6 +47,7 @@ class StopAndWaitHost(Host, ABC):
         packets_received = self.network_interface.receive_all()
         if packets_received and packets_received[0].sequence_number == self.next_up:
             self.acked.append (packets_received[0])
+            self.inflight.clear()
             print ("acked")
             self.next_up += 1
 
